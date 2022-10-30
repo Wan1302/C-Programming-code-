90ae1edbd4a49dd859f1b730a64ace788a029ab5
DuyManzzz <117023051+DuyManzzz@users.noreply.github.com>
2022-10-30 21:54:37 +0700

    Update man.cpp

--- man.cpp
+++ man.cpp
@@ -1,3 +1,5 @@
+
+//VD1
 // #include <iostream>
 // using namespace std;
 
@@ -13,17 +15,35 @@
 //     return 0;
 // }
 
+//VD2
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++) {
+//         if (n % i == 0) {
+//             cout << i << "  " ;
+//         }
+
+//     }
+// return 0;
+// }
+ 
+//VD3 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n;
+    int n, i = 0;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            cout << i << "  " ;
-        }
 
-    }
-return 0;
-}
+    while (n >= 1) {
+            n = n/10;
+            i++;
+        }
+    cout << i;
+    return 0;
+    }    
+
